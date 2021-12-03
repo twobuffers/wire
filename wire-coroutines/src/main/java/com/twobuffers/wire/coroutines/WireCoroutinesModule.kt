79633163c86a18fff2cb0cf005ba382d9b76f1e7
Greg Rynkowski <5878299+rynkowsg@users.noreply.github.com>
2021-12-03 05:17:53 +0000

    Add WireCoroutinesCommonModule

--- wire-coroutines/src/main/java/com/twobuffers/wire/coroutines/WireCoroutinesModule.kt
+++ wire-coroutines/src/main/java/com/twobuffers/wire/coroutines/WireCoroutinesModule.kt
@@ -4,31 +4,10 @@ import androidx.lifecycle.ProcessLifecycleOwner
 import androidx.lifecycle.coroutineScope
 import dagger.Module
 import dagger.Provides
-import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
 
-@Module
-object WireAsyncCoroutinesModule {
-    @Provides
-    @ComputationDispatcher
-    fun provideComputationDispatcher(): CoroutineDispatcher = Dispatchers.Default
-
-    @Provides
-    @IoDispatcher
-    fun provideIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
-
-    @Provides
-    @MainDispatcher
-    fun provideMainDispatcher(): CoroutineDispatcher = Dispatchers.Main
-
-    @DelicateCoroutinesApi
-    @Provides
-    @GlobalCoroutineScope
-    fun provideGlobalCoroutineScope(): CoroutineScope = GlobalScope
-
+@Module(includes = [WireCoroutinesCommonModule::class])
+object WireCoroutinesModule {
     @Provides
     @ProcessLifetimeCoroutineScope
     fun provideProcessLifetimeCoroutineScope(): CoroutineScope =
