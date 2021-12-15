ac1382f4941e212103035a2f2e6deae0530f16e1
Greg Rynkowski <5878299+rynkowsg@users.noreply.github.com>
2021-12-15 10:30:51 +0000

    Add minor improvements in CoroutinesExt

--- wire-coroutines-common/src/main/java/com/twobuffers/wire/coroutines/CoroutinesExt.kt
+++ wire-coroutines-common/src/main/java/com/twobuffers/wire/coroutines/CoroutinesExt.kt
@@ -1,6 +1,7 @@
 package com.twobuffers.wire.coroutines
 
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.currentCoroutineContext
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
@@ -9,8 +10,8 @@ import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 
 fun CoroutineScope.launchPeriodically(
-    initialDelay: Long,
     repeatMillis: Long,
+    initialDelay: Long = 0L,
     block: suspend () -> Unit
 ) = launch {
     delay(initialDelay)
@@ -21,14 +22,15 @@ fun CoroutineScope.launchPeriodically(
 }
 
 @Suppress("NOTHING_TO_INLINE")
-inline fun CoroutineScope.every(repeatMillis: Long, initialDelay: Long = 0, noinline block: suspend () -> Unit) =
+inline fun CoroutineScope.every(repeatMillis: Long, initialDelay: Long = 0, noinline block: suspend () -> Unit): Job =
     launchPeriodically(initialDelay = initialDelay, repeatMillis = repeatMillis, block = block)
 
 @Suppress("NOTHING_TO_INLINE")
-inline fun CoroutineScope.everyHour(initialDelay: Long = 0, noinline block: suspend () -> Unit) =
+inline fun CoroutineScope.everyHour(initialDelay: Long = 0, noinline block: suspend () -> Unit): Job =
     every(initialDelay, 60 * 60_000L, block)
 
-fun every(repeatMillis: Long): Flow<Long> = flow {
+fun every(repeatMillis: Long, initialDelay: Long = 0): Flow<Long> = flow {
+    delay(initialDelay)
     while (currentCoroutineContext().isActive) {
         emit(System.currentTimeMillis())
         delay(repeatMillis)
