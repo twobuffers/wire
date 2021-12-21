2919002f3384d068285f2f60f6d5eb12adc0f886
Greg Rynkowski <5878299+rynkowsg@users.noreply.github.com>
2021-12-21 20:35:18 +0000

    Add better wiring for build.gradle.kts files

---
+++ buildSrc/src/main/kotlin/GradlePluginId.kt
@@ -0,0 +1,8 @@
+object GradlePluginId {
+    const val ANDROID_APPLICATION = "com.android.application"
+    const val ANDROID_LIBRARY = "com.android.library"
+    const val KOTLIN_ANDROID = "org.jetbrains.kotlin.android"
+    const val KOTLIN_JVM = "org.jetbrains.kotlin.jvm"
+    const val KOTLIN_KAPT = "org.jetbrains.kotlin.kapt"
+    const val MAVEN_PUBLISH = "com.vanniktech.maven.publish"
+}
