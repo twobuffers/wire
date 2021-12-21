eb86282ef8e45fd7efb086419a25c125e60335cd
Greg Rynkowski <5878299+rynkowsg@users.noreply.github.com>
2021-12-21 20:59:30 +0000

    Downgrade minSdkVersion to 21

--- buildSrc/src/main/kotlin/AndroidConfig.kt
+++ buildSrc/src/main/kotlin/AndroidConfig.kt
@@ -1,6 +1,6 @@
 object AndroidConfig {
     const val COMPILE_SDK_VERSION = 31
-    const val MIN_SDK_VERSION = 25
+    const val MIN_SDK_VERSION = 21
     const val TARGET_SDK_VERSION = 30
     const val BUILD_TOOLS_VERSION = "31.0.0"
 }
