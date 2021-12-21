20bcf7fc3d20ff6f48ad586260f8225c8420acb5
Greg Rynkowski <5878299+rynkowsg@users.noreply.github.com>
2021-12-21 17:13:32 +0000

    Add onViewDestroy, useful for deinit of view elements

--- wire-utils-android/src/main/java/com/twobuffers/wire/utils/android/LifecycleExt.kt
+++ wire-utils-android/src/main/java/com/twobuffers/wire/utils/android/LifecycleExt.kt
@@ -2,9 +2,24 @@ package com.twobuffers.wire.utils.android
 
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
+
+// Regular
+
+fun Lifecycle.onEvent(event: Lifecycle.Event, block: (source: LifecycleOwner) -> Unit) {
+    val o = LifecycleEventObserver { source, receivedEvent -> if (receivedEvent == event) block(source) }
+    addObserver(o)
+}
+
+fun Fragment.onViewDestroy(block: (source: LifecycleOwner) -> Unit) =
+    viewLifecycleOwner.lifecycle.onEvent(Lifecycle.Event.ON_DESTROY, block)
+
+// Coroutines
 
 fun AppCompatActivity.launchWhenCreated(
     block: suspend CoroutineScope.() -> Unit
