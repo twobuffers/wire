4dc274c50cdbddd7456e089f9491f842ad2d597a
Greg Rynkowski <5878299+rynkowsg@users.noreply.github.com>
2022-01-26 15:15:09 +0000

    Add Fragment.clearAdapterOnViewDestroy

--- wire-utils-android/src/main/java/com/twobuffers/wire/utils/android/LifecycleExt.kt
+++ wire-utils-android/src/main/java/com/twobuffers/wire/utils/android/LifecycleExt.kt
@@ -3,21 +3,24 @@ package com.twobuffers.wire.utils.android
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.Lifecycle.Event
 import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
 
 // Regular
 
-fun Lifecycle.onEvent(event: Lifecycle.Event, block: (source: LifecycleOwner) -> Unit) {
+fun Lifecycle.onEvent(event: Event, block: (source: LifecycleOwner) -> Unit) {
     val o = LifecycleEventObserver { source, receivedEvent -> if (receivedEvent == event) block(source) }
     addObserver(o)
 }
 
 fun Fragment.onViewDestroy(block: (source: LifecycleOwner) -> Unit) =
-    viewLifecycleOwner.lifecycle.onEvent(Lifecycle.Event.ON_DESTROY, block)
+    viewLifecycleOwner.lifecycle.onEvent(Event.ON_DESTROY, block)
 
 // Coroutines
 
@@ -28,3 +31,11 @@ fun AppCompatActivity.launchWhenCreated(
 fun Fragment.launchWhenCreated(
     block: suspend CoroutineScope.() -> Unit
 ): Job = lifecycleScope.launchWhenCreated(block = block)
+
+// Utilities
+
+fun RecyclerView.clearAdapterOnViewDestroy(viewLifecycleOwner: LifecycleOwner) =
+    viewLifecycleOwner.lifecycle.onEvent(Event.ON_DESTROY) { adapter = null }
+
+fun ViewPager2.clearAdapterOnViewDestroy(viewLifecycleOwner: LifecycleOwner) =
+    viewLifecycleOwner.lifecycle.onEvent(Event.ON_DESTROY) { adapter = null }
