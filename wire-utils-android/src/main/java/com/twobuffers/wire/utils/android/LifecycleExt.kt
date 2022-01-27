cd3d758f61a4a1c61521e5c5dc74de1ad4a7e65d
Greg Rynkowski <5878299+rynkowsg@users.noreply.github.com>
2022-01-27 22:24:50 +0000

    Refactor and correct Lifecycle utils

--- wire-utils-android/src/main/java/com/twobuffers/wire/utils/android/LifecycleExt.kt
+++ wire-utils-android/src/main/java/com/twobuffers/wire/utils/android/LifecycleExt.kt
@@ -1,9 +1,12 @@
+@file:Suppress("NOTHING_TO_INLINE")
+
 package com.twobuffers.wire.utils.android
 
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.Lifecycle.Event
+import androidx.lifecycle.Lifecycle.Event.ON_DESTROY
 import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.lifecycleScope
@@ -19,8 +22,14 @@ fun Lifecycle.onEvent(event: Event, block: (source: LifecycleOwner) -> Unit) {
     addObserver(o)
 }
 
-fun Fragment.onViewDestroy(block: (source: LifecycleOwner) -> Unit) =
-    viewLifecycleOwner.lifecycle.onEvent(Event.ON_DESTROY, block)
+inline fun LifecycleOwner.onEvent(event: Event, noinline block: (source: LifecycleOwner) -> Unit) =
+    lifecycle.onEvent(event, block)
+
+inline fun Fragment.onViewEvent(event: Event, noinline block: (source: LifecycleOwner) -> Unit) =
+    viewLifecycleOwner.onEvent(event, block)
+
+inline fun LifecycleOwner.onDestroy(noinline block: (source: LifecycleOwner) -> Unit) = onEvent(ON_DESTROY, block)
+inline fun Fragment.onViewDestroy(noinline block: (source: LifecycleOwner) -> Unit) = onViewEvent(ON_DESTROY, block)
 
 // Coroutines
 
@@ -34,8 +43,13 @@ fun Fragment.launchWhenCreated(
 
 // Utilities
 
-fun RecyclerView.clearAdapterOnViewDestroy(viewLifecycleOwner: LifecycleOwner) =
-    viewLifecycleOwner.lifecycle.onEvent(Event.ON_DESTROY) { adapter = null }
+fun RecyclerView.clearAdapterOnDestroyOf(lo: LifecycleOwner) = lo.onDestroy { adapter = null }
+fun ViewPager2.clearAdapterOnDestroyOf(lo: LifecycleOwner) = lo.onDestroy { adapter = null }
+fun RecyclerView.clearAdapterOnViewDestroyOf(fragment: Fragment) = fragment.onViewDestroy { adapter = null }
+fun ViewPager2.clearAdapterOnViewDestroyOf(fragment: Fragment) = fragment.onViewDestroy { adapter = null }
 
-fun ViewPager2.clearAdapterOnViewDestroy(viewLifecycleOwner: LifecycleOwner) =
-    viewLifecycleOwner.lifecycle.onEvent(Event.ON_DESTROY) { adapter = null }
+@Deprecated("Method deprecated", ReplaceWith("clearAdapterOnViewDestroyOf(fragment)"))
+fun RecyclerView.clearAdapterOnViewDestroy(fragment: Fragment) = clearAdapterOnViewDestroyOf(fragment)
+
+@Deprecated("Method deprecated", ReplaceWith("clearAdapterOnViewDestroyOf(fragment)"))
+fun ViewPager2.clearAdapterOnViewDestroy(fragment: Fragment) = clearAdapterOnViewDestroyOf(fragment)
