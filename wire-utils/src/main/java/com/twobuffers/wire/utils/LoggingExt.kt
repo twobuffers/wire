efd3327099db163615b4c1938463c4f3a64ec3ff
Greg Rynkowski <5878299+rynkowsg@users.noreply.github.com>
2022-01-27 21:53:15 +0000

    Add Any.address

--- wire-utils/src/main/java/com/twobuffers/wire/utils/LoggingExt.kt
+++ wire-utils/src/main/java/com/twobuffers/wire/utils/LoggingExt.kt
@@ -3,3 +3,8 @@ package com.twobuffers.wire.utils
 // https://stackoverflow.com/questions/34122450/how-to-get-generic-parameter-class-in-kotlin/34123047#34123047
 inline val <reified T : Any>T.className get() = T::class.java.simpleName
 inline val <reified T : Any>T.logTag get() = T::class.java.simpleName
+
+// Create 'address' of the object, an alternative to `toString` method.
+// There the 'address' is class name combined with the instance memory address.
+// Example result: `MainActivity@11ed7e8`. It is handy for printing objects to stdout.
+inline val <T : Any> T.address get(): String = "${this::class.java.simpleName}@${hashCode().toString(16)}"
